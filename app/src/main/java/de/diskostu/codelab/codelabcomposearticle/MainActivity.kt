2bb138e78e84602e4b7466ee20d66291cb638732
Denny Kluge <denny.kluge@gmail.com>
2023-01-16 09:08:09 +0100

    finished codelab

--- app/src/main/java/de/diskostu/codelab/codelabcomposearticle/MainActivity.kt
+++ app/src/main/java/de/diskostu/codelab/codelabcomposearticle/MainActivity.kt
@@ -3,13 +3,22 @@ package de.diskostu.codelab.codelabcomposearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import de.diskostu.codelab.codelabcomposearticle.ui.theme.CodelabComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +28,7 @@ class MainActivity : ComponentActivity() {
             CodelabComposeArticleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -27,14 +36,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = stringResource(R.string.headline),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(all = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.paragraph1),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.paragraph2),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(all = 16.dp)
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CodelabComposeArticleTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
